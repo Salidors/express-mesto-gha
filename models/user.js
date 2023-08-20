b3df66e36bdfb04c1832f4c26c359623effe22bd
Abramova Nina <abramova.nina.g@gmail.com>
2023-08-20 18:39:45 +0300

    feat: add schema user & validator

--- models/user.js
+++ models/user.js
@@ -1,22 +1,39 @@
 const mongoose = require('mongoose');
+const { default: isEmail } = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
+    default: 'Исследователь',
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    required: false,
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    required: [true, 'Неоюходимо заполнить Email'],
+    validator: isEmail('foo@bar.com'),
+  },
+  password: {
+    type: String,
+    required: [true, 'Неоюходимо заполнить Email'], // hash
   },
 });
+
+//   var validator = require('validator');
+// validator.isEmail('foo@bar.com');
 
 module.exports = mongoose.model('user', userSchema);
