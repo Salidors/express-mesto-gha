626c78b14adb738a4021e60197e30c992b79ae54
Abramova Nina <abramova.nina.g@gmail.com>
2023-08-26 22:13:58 +0300

    chore: linter

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res
     .status(err.statusCode || 500)
     .send({ message: err.message || 'Что-то случилось...' });
